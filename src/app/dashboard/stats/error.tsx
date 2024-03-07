2528a12824e10ef65c4132b86b5e11d13f65aec3
rohank36 <rohan.kanti@mail.mcgill.ca>
2024-03-07 00:06:26 -0500

    Added files

--- src/app/dashboard/stats/error.tsx
+++ src/app/dashboard/stats/error.tsx
@@ -1,4 +1,5 @@
 'use client'
+//TODO: Style error properly
 export default function Error() {
     return <p>error...</p>
 }
--- src/app/dashboard/stats/loading.tsx
+++ src/app/dashboard/stats/loading.tsx
@@ -1,3 +1,4 @@
+//TODO: Style loading properly
 export default function Loading(){
     return <p>Loading...</p>
 }

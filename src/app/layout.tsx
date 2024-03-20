fa4428bd8a69e8acdcdf6eedce1f3e2a467c356d
rohank36 <rohan.kanti@mail.mcgill.ca>
2024-03-20 00:18:10 -0400

    Added tailwind and dasiy ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import styles from "./page.module.css";
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,7 +20,11 @@ export default function RootLayout({
       <UserProvider>
         <body className={inter.className}>
           {children}
-          <p className={styles.love}>Made with &#x1F499; by Rohan</p>
+          <footer className="footer footer-center p-4 bg-base-300 text-base-content absolute bottom-0 w-full">
+            <aside>
+              <p>Copyright © 2024 - All rights reserved by The High Council Of Intellectuals - Made with &#x1F499; by Rohan</p>
+            </aside>
+          </footer>
         </body>
       </UserProvider>
     </html>
